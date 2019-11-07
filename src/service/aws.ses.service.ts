7e7203f61a03109085613fb95a6aa64dc565cc42
Med Amine Abbes <abbesmedamine52@gmail.com>
2019-11-07 14:54:46 +0100

    fix(aws.sns.service): response body
    fix response body

--- src/service/aws.ses.service.ts
+++ src/service/aws.ses.service.ts
@@ -1,80 +1,43 @@
 'use strict';
-import { Injectable, Inject, Logger, HttpStatus } from '@nestjs/common';
-import { CONFIG_CONNECTION_OPTIONS } from '../constants';
-import { Attachment } from 'nodemailer/lib/mailer';
-import { Email } from 'aws-sdk/clients/connect';
+import { HttpStatus, HttpException, Inject, Injectable, Logger } from '@nestjs/common';
+import { createTransport, SendMailOptions, SentMessageInfo, Transporter } from 'nodemailer';
 import { Options } from 'nodemailer/lib/smtp-connection';
-import {
-    Transporter,
-    SendMailOptions,
-    createTransport,
-    SentMessageInfo,
-} from 'nodemailer';
+
+import { CONFIG_CONNECTION_OPTIONS } from '../constants';
+
 @Injectable()
 export class AwsSesService {
     private readonly _transporter: Transporter;
-    private _mailOptions: SendMailOptions;
 
     constructor(@Inject(CONFIG_CONNECTION_OPTIONS) private _options: Options) {
         Logger.log('initialising AWS Module', 'SES SERVICE');
         // create reusable transporter object using the default SMTP transport
         this._transporter = createTransport(this._options);
     }
+
     /**
      *
-     *
-     * @param {string} recipientAddress
-     * @param {string} subjectOfMail
-     * @param {string} contentOfMail
-     * @param {string} template
-     * @param {string} [senderAddress]
-     * @param {string[]} [attachement]
-     * @returns {Promise<AwsMailDetails>}
-     * @memberof AwsSesService
+     * @param {SendMailOptions} mailOptions
      */
-    async sendMail(
-        recipientAddress: Email,
-        subjectOfMail: string,
-        contentOfMail: string,
-        template: string,
-        attachment?: Attachment[],
-        senderAddress?: Email,
-    ) {
-        // setup e-mail data with unicode symbols
-        this._mailOptions = {
-            from: senderAddress,
-            to: recipientAddress, //  'bar@example.com, baz@example.com', here you can set more then one reciver address
-            subject: subjectOfMail, //   Subject line
-            text: contentOfMail, //    plain text body
-            html: template, //     html body
-            attachments: attachment,
-        };
-
-        return [
-            // promise send mail
-            await this._transporter
-                .sendMail(this._mailOptions)
-                .then((info: SentMessageInfo) => {
-                    console.log('RESPONSE MAIL DETAILS ====>', info);
-                    return [
-                        {
-                            success: HttpStatus.OK,
-                            message: 'MAIL SUCCESSFULLY SENDED',
-                            data: info,
-                        },
-                    ];
-                })
-                .catch((err) => {
-                    console.log('ERROR : FAILD REQUEST!!====>', err);
-                    return [
-                        {
-                            error: HttpStatus.EXPECTATION_FAILED,
-                            message: ['FAILD TO SEND MAIL', err],
-                        },
-                    ];
-                }),
-        ];
-        //  console.log('Message sent: %s', info.messageId);
-        // console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+    async sendMail(mailOptions: SendMailOptions) {
+        // promise send mail
+        return this._transporter
+            .sendMail(mailOptions)
+            .then((info: SentMessageInfo) => {
+                Logger.log('success[sendMail]:', info);
+                return {
+                    statusCode: HttpStatus.OK,
+                    message: 'Mail Sent',
+                    data: info,
+                };
+            })
+            .catch((err) => {
+                Logger.log('error[sendMail]:', err);
+                throw new HttpException({
+                    statusCode: HttpStatus.BAD_REQUEST,
+                    message: 'Failed to send',
+                    data: err,
+                }, HttpStatus.BAD_REQUEST);
+            });
     }
 }
--- src/service/aws.sns.service.ts
+++ src/service/aws.sns.service.ts
@@ -1,16 +1,16 @@
-import { Injectable, Inject, Logger, HttpStatus } from '@nestjs/common';
-import * as AWS from 'aws-sdk';
+import { Injectable, Inject, Logger, HttpStatus, HttpException } from '@nestjs/common';
 import { CONFIG_CONNECTION_OPTIONS } from '../constants';
 import { ConfigurationOptions } from 'aws-sdk/lib/config';
-import { PublishResponse } from 'aws-sdk/clients/sns';
+import { PublishResponse, PublishInput } from 'aws-sdk/clients/sns';
+import AWS, { SNS } from 'aws-sdk';
+
 /**
  * @export
  * @class AwsSnsService
  */
 @Injectable()
 export class AwsSnsService {
-    private readonly _sns: AWS.SNS;
-    private _smsOptions: AWS.SNS.PublishInput;
+    private readonly _sns: SNS;
     constructor(
         @Inject(CONFIG_CONNECTION_OPTIONS)
         private _options: ConfigurationOptions,
@@ -20,49 +20,28 @@ export class AwsSnsService {
         this._sns = new AWS.SNS();
     }
 
-    /**
-     *
-     * SNS AMAZON SEND SMS
-     *
-     * @param {string} mobileNumber
-     * @param {string} messageToSend
-     * @param {string} subjectOfSms
-     * @memberof AwsSnsService
-     */
-    async sendSMS(
-        mobileNumber: string,
-        messageToSend: string,
-        subjectOfSms: string,
-    ) {
-        this._smsOptions = {
-            Message: messageToSend,
-            Subject: subjectOfSms,
-            PhoneNumber: mobileNumber,
-        };
+    async sendSMS(smsOptions: PublishInput) {
 
-        return [
-            await this._sns
-                .publish(this._smsOptions)
+        return this._sns
+                .publish(smsOptions)
                 .promise()
                 .then((info: PublishResponse) => {
-                    console.log('RESPONSE SMS DETAILS ====>', info);
+                    Logger.log(` success[sendSms]: ${JSON.stringify(info)}`);
                     return [
                         {
-                            success: HttpStatus.OK,
-                            message: 'SMS SUCCESSFULLY SENDED',
+                            statusCode: HttpStatus.OK,
+                            message: 'Sms sent',
                             data: info,
                         },
                     ];
                 })
-                .catch(err => {
-                    console.log('ERROR : FAILD REQUEST !! ====>', err);
-                    return [
-                        {
-                            error: HttpStatus.EXPECTATION_FAILED,
-                            message: ['FAILD TO SEND SMS ', err],
-                        },
-                    ];
-                }),
-        ];
+                .catch((err) => {
+                    Logger.error('error[sendSms]:', err);
+                    throw new HttpException({
+                        statusCode: HttpStatus.BAD_REQUEST,
+                        message: 'Failed to send',
+                        data: err,
+                    }, HttpStatus.BAD_REQUEST);
+                });
     }
 }
